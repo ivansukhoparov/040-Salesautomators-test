d079a703b1a0ba96419be8e20f13d526b31ba038
ivansukhoparov <rbcdaaa@gmail.com>
2024-09-21 21:53:12 +0200

    checking vercel update

--- src/routers/home.router.ts
+++ src/routers/home.router.ts
@@ -1,9 +1,9 @@
 import {Request, Response, Router} from "express";
+import {appVersion} from "../version";
 
-const version = "0.0.4"
 export const homeRouter = Router()
 
 homeRouter.get("/", async (req: Request, res: Response) => {
-    const responseMsg = `this is test task for Salesautomators v.${version}`
+    const responseMsg = `this is test task for Salesautomators v.${appVersion}`
     res.status(200).json(responseMsg);
 })
---
+++ src/version.ts
@@ -0,0 +1,2 @@
+
+export const appVersion = "0.0.5"
