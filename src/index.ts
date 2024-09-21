c41536d1a9cfe996c5c084cf07377df1320a5883
ivansukhoparov <rbcdaaa@gmail.com>
2024-09-21 21:49:06 +0200

    checking vercel update

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
-import express from "express"
 import {app} from "./app";
-export const version = "0.0.4"
+
+export const version = () => {
+    return "0.0.4"
+}
 
 const port = 3002
 
-app.listen(port, async ()=>{
+app.listen(port, async () => {
     console.log(`app start on port ${port}`);
     console.log(`open in browser http://localhost:${port}`);
 })
