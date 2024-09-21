675551cc713919b5d1405b13f2428ac72e197d2d
ivansukhoparov <rbcdaaa@gmail.com>
2024-09-21 21:40:06 +0200

    checking vercel

--- src/routers/home.router.ts
+++ src/routers/home.router.ts
@@ -4,5 +4,5 @@ export const homeRouter = Router()
 
 homeRouter.get("/", async (req: Request, res: Response) => {
     console.log("s")
-    res.status(200).json("this is test task for Salesautomators v.0.0.1");
+    res.status(200).json("this is test task for Salesautomators v.0.0.2");
 })
