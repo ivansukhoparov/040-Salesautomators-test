4566f7cceb6c9cd14401952a04c2d79ef59385ba
ivansukhoparov <rbcdaaa@gmail.com>
2024-09-22 15:35:20 +0200

    add cors headers

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,23 @@
-import express from "express";
+import express, {NextFunction,Response,Request} from "express";
 import {homeRouter} from "./routers/home.router";
 import {authRouter} from "./routers/auth.router";
 import {jobsRouter} from "./routers/jobs.router";
 
-export const app = express()
-app.use(express.json())
-app.use("/", homeRouter)
-app.use("/auth", authRouter)
-app.use("/jobs", jobsRouter)
+
+    export const app = express()
+    app.use(express.json())
+    app.use((req: Request, res: Response, next: NextFunction) => {
+        res.setHeader('Access-Control-Allow-Origin', '*')
+            .setHeader('Access-Control-Request-Method', '*')
+            .setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET,POST,PUT,DELETE')
+            .setHeader('Access-Control-Allow-Headers', '*')
+        next()
+    })
+// app.use(
+//
+//
+// )
+    app.use("/", homeRouter)
+    app.use("/auth", authRouter)
+    app.use("/jobs", jobsRouter)
+
