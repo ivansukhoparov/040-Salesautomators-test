a48629e1b8a485091857752ac3469c1813010461
ivansukhoparov <rbcdaaa@gmail.com>
2024-09-25 13:02:48 +0200

    add job types

---
+++ src/types/job.types.ts
@@ -0,0 +1,18 @@
+export type newJobRawType = {
+    first_name: string
+    last_name: string
+    phone: string
+    email: string
+    job_type: string
+    job_source: string
+    job_description: string
+    address: string
+    city: string
+    state: string
+    zip_code: string
+    area: string
+    date: string
+    start_time: string
+    end_time: string
+    test: string
+}
